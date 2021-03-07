3c7faf83004fd7a4ec5e8adf1c6a692b74446e5a
Michael Jumper <mjumper@apache.org>
2021-03-06 17:15:45 -0800

    WIP: Pull Broadway library from git during build (rather than embed in source).

--- extensions/guacamole-video-h264/src/main/resources/js/loadBroadway.js
+++ extensions/guacamole-video-h264/src/main/resources/js/loadBroadway.js
@@ -1,2 +1,2 @@
 // Define path to bundled Broadway H264 library
-Guacamole.H264VideoPlayer.BROADWAY_PLAYER_PATH = 'app/ext/guac-h264/lib';
+Guacamole.H264VideoPlayer.BROADWAY_PLAYER_PATH = 'app/ext/guac-h264/lib/broadway';
